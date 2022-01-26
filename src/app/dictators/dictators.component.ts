28420d36701f592a4463cc55c15b7e99e03079a4
Thomas Carl Alexander Dänhardt <thomas.danhardt@gmail.com>
2022-01-26 14:40:05 +0100

    small adjustments

--- src/app/dictators/dictators.component.ts
+++ src/app/dictators/dictators.component.ts
@@ -11,6 +11,7 @@ export class DictatorsComponent implements OnInit {
   dictators: Dictator[] = [
     new Dictator('Dicktator', 'DIck potatoe lol', ['being a dick','being a potatoe'], [new Color(255, 200, 200, 1), new Color(200, 125, 125, 1), new Color(75, 0, 0, 1)], 'dicktatoe.png', 'NaziMidi.mp3'),
     new Dictator('Dicktamatoe', 'Tomatoe Dick lolmao', ['being a tomatoe','being a dick'], [new Color(200, 255, 200, 1), new Color(125, 200, 125, 1), new Color(50, 50, 50, 1)], 'tomator.png', 'tomato.mp3'),
+    new Dictator('The Onionizer', 'Iron-fisted leader of the Soyviet Onion', ['Onionizing', 'Making people cry', 'being a fail idiotlogy haha take that jacob and andreas lol commies wrecked again'], [new Color(255, 0, 0, 1), new Color(125, 0, 0, 1), new Color(255, 200, 200, 1)], 'onionizer.png', 'soyvietOnionMarch.mp3')
   ];
 
   constructor() { }
